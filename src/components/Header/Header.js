207055a867c7a63286932528f2c29a755f5beb65
Rajukst <rajuahamedkst15@gmail.com>
2021-09-26 21:44:02 +0600

    code update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,9 @@ const Header = () => {
     return (
         <div className='h-component'>
             <div className="header-section">
-                <h1>Nilima Electronic <span>Wellcome</span> </h1>
-                <h2>Wellcome to Nilima Electronics shop</h2>
+                <h1>Wellcome <span>Nilima Electronic</span> </h1>
+                <p>At select Best Buy stores, we can repair your out-of-warranty iPhone or Samsung phone screen, no matter where you bought it.</p>
+                <h2>Total Budget: 100 Million</h2>
             </div>
             <div className="myCart">
                 <MyShop></MyShop>
--- src/components/MyCart/Cart.js
+++ src/components/MyCart/Cart.js
@@ -6,15 +6,22 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 const element = <FontAwesomeIcon icon={faUser} />
 const Cart = (props) => {
     const { getCart } = props;
+    console.log(getCart)
     // calculating total amount
     let initialAmount = 0;
+    let productCount = '';
     for (let newAmount of getCart) {
         initialAmount = initialAmount + newAmount.price;
+        productCount = productCount + newAmount.name
         console.log(initialAmount)
     }
     return (
         <div className='cart-component'>
             <h1>{element} Your Cart Summary</h1>
+            <div className='pragraph'>
+                <p>Product Count: {productCount}</p>
+            </div>
+
             <h3> Total Product Added: {props.getCart.length} </h3>
             <h4> Total Cost: {initialAmount.toFixed(2)}</h4>
         </div>
